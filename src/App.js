0d168c620645da5ee4e815e4096e36acfe1ddf71
Gabe <gabrielsanchez@my.unt.edu>
2023-01-30 00:07:01 -0600

    Removed SignUp Page

--- src/App.js
+++ src/App.js
@@ -2,19 +2,28 @@ import './App.css';
 
 import { Amplify } from 'aws-amplify';
 
-import { withAuthenticator } from '@aws-amplify/ui-react';
+import { Authenticator} from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 
 import awsExports from './aws-exports';
 Amplify.configure(awsExports);
 
-function App({ signOut, user }) {
-  return (
-    <>
-      <h1>Hello {user.username}</h1>
-      <button onClick={signOut}>Sign out</button>
-    </>
-  );
+
+const styles = {
+  container: { width: 750, margin: '0 auto', display: 'flex', flexDirection: 'column', justifyContent: 'center', padding: 100 },
 }
 
-export default withAuthenticator(App);
+export default function App(){
+  return (
+    <div style={styles.container}>
+    <Authenticator hideSignUp={true}>
+    {({ signOut, user }) => (
+      <main>
+        <h1>Hello {user.username}</h1>
+        <button onClick={signOut}>Sign out</button>
+      </main>
+    )}
+    </Authenticator>
+    </div>
+  );
+}
