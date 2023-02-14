b214d46b6954578de8d3bf629becb5fbd580709b
gabsanchez7 <65166522+gabsanchez7@users.noreply.github.com>
2023-02-13 18:06:42 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,28 +2,37 @@ import './App.css';
 
 import { Amplify } from 'aws-amplify';
 
-import { Authenticator} from '@aws-amplify/ui-react';
+
+import {Authenticator, View, Image, useTheme} from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 
 import awsExports from './aws-exports';
 Amplify.configure(awsExports);
 
+const components = {
+  Header() {
+    const { tokens } = useTheme();
 
-const styles = {
-  container: {padding: 100 },
-}
+    return (
+      <View textAlign="center" padding={tokens.space.large}>
+        <Image
+          alt="Adavanced Intralogistics logo"
+          src="/img/AI_logo.png"
+        />
+      </View>
+    );
+  }
+};
 
 export default function App(){
   return (
-    <div style={styles.container}>
-    <Authenticator hideSignUp={true}>
-    {({ signOut, user }) => (
-      <main>
-        <h1>Hello {user.username}</h1>
-        <button onClick={signOut}>Sign out</button>
-      </main>
-    )}
+    <Authenticator hideSignUp={true} components={components}>
+      {({ signOut, user }) => (
+        <main>
+          <h1>Hello {user.username}</h1>
+          <button onClick={signOut}>Sign out</button>
+        </main>
+      )}
     </Authenticator>
-    </div>
   );
 }
