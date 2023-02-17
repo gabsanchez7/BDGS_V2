3f7247cbd8bb5e2b0eec32091e130f2924d2f45e
Gabe <gabrielsanchez@my.unt.edu>
2023-02-16 23:41:22 -0600

    Messing with customization changed the icon for the tab and change the name that appears

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import './App.css';
 import logo from './img/AI_logo.png';
 import { Amplify } from 'aws-amplify';
 
-import {Authenticator, View, Image, useTheme, Heading} from '@aws-amplify/ui-react';
+
+import {Authenticator, View, Image, useTheme, Heading, Button, Flex, ThemeProvider} from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 
 import awsExports from './aws-exports';
@@ -13,9 +14,9 @@ const components = {
     const { tokens } = useTheme();
 
     return (
-      <View textAlign="center" padding={tokens.space.large}>
-        <Image width="50%"
-          height="50%"
+      <View textAlign="center" padding={tokens.space.small}>
+        <Image width="40%"
+          height="40%"
           alt="Adavanced Intralogistics logo"
           src={logo}
         />
@@ -38,15 +39,37 @@ const components = {
   },
 };
 
+const myTheme = {
+  name: 'my-theme',
+  tokens: {
+    colors: {
+      background: {
+        primary: { value: '{colors.neutral.90.value}'},
+        secondary: { value: '{colors.neutral.100.value}'},
+      },
+      font: {
+        // references colors.neutral.100
+        // because the default theme defines that color already
+        // we don't need to re-define it here
+        primary: { value: '{colors.neutral.100.value}' },
+      },
+    },
+  },
+};
+
 export default function App(){
   return (
+    <ThemeProvider theme={myTheme}>
     <Authenticator hideSignUp={true} components={components}>
-      {({ signOut, user }) => (
-        <main>
-          <h1>Hello {user.username}</h1>
-          <button onClick={signOut}>Sign out</button>
-        </main>
-      )}
+        {({ signOut, user }) => (
+          <main>
+            <Flex justifyContent={'right'} size>
+              <Button onClick={signOut}>Sign out</Button>
+            </Flex>
+            <h2>Hello {user.attributes.email}</h2>
+          </main>
+        )}
     </Authenticator>
+    </ThemeProvider>
   );
 }
