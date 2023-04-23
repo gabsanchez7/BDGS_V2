d5ab137796191972a70e075c0c745b9487ef5506
Gabe <gabrielsanchez@my.unt.edu>
2023-04-22 23:14:00 -0500

    Add cloudflare to homeassistant to fix iframe issue

--- src/App.js
+++ src/App.js
@@ -145,7 +145,7 @@ export default function App(){
                 rowEnd="50"
               >
               <flex>
-              <iframe src="https://www.google.com" name="HOMEAPP" allowTransparency="true" width="100%" height="100%" >
+              <iframe src="https://homeassistantaihomeapp.uk/" name="HOMEAPP" allowTransparency="true" width="100%" height="100%" >
               </iframe>
               </flex>
               </Card>
